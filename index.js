48bddbd9ed0e49c65150325f2d55f4dd2a983ff1
Elkhan <elkhan.safarov10@yandex.ru>
2019-10-29 21:30:25 +0500

    Отправка картинок

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 const debug = require('./helpers')
+const fs = require('fs')
 const TOKEN = '917940363:AAF020LhXPLehaH3Q4JoiMOyGw93oVzayIw'
 
 console.log('Бот успешно запушен')
@@ -12,54 +13,10 @@ const bot = new TelegramBot(TOKEN, {
 			timeout: 10
 		}
 	}})
+ 
 
-const inline_keyboard = [
-	[
-		{
-			text: 'Forward',
-			callback_data: 'forward'
-		},
-		{
-			text: 'Reply',
-			callback_data: 'reply'
-		}
-	],
-	[
-		{
-			text: 'Edit',
-			callback_data: 'edit'
-		},
-		{
-			text: 'Delete',
-			callback_data: 'delete'
-		}
-	]
-]
-
-bot.on('callback_query', query => {
-	const { chat, message_id, text } = query.message
-
-	switch (query.data) {
-		case 'forward':
-			bot.forwardMessage(chat.id, chat.id, message_id)
-			break
-		case 'reply':
-			bot.sendMessage(chat.id, `Отвечаем на сообщение`, {
-				reply_to_message_id: message_id
-			})
-			break
-	}
-	bot.answerCallbackQuery({
-		callback_query_id: query.id
-	})
-})
-
-bot.onText(/\/start/, (msg, [source, match]) => {
-	const chatId = msg.chat.id
-
-	bot.sendMessage(chatId, 'Keyboard', {
-		reply_markup: {
-			inline_keyboard
-		}
+bot.onText(/\/pic2/, msg => {
+	bot.sendPhoto(msg.chat.id, './140777471_2.jpg', {
+		caption: 'This is cat photo'
 	})
 })
