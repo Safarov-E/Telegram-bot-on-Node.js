05712ae667a0f557ec1674352473788d0bccc1ee
Elkhan <elkhan.safarov10@yandex.ru>
2019-10-29 17:16:26 +0500

    Ответ на сообщение

--- index.js
+++ index.js
@@ -13,21 +13,53 @@ const bot = new TelegramBot(TOKEN, {
 		}
 	}})
 
-bot.on('inline_query', query => {
-	const results = []
+const inline_keyboard = [
+	[
+		{
+			text: 'Forward',
+			callback_data: 'forward'
+		},
+		{
+			text: 'Reply',
+			callback_data: 'reply'
+		}
+	],
+	[
+		{
+			text: 'Edit',
+			callback_data: 'edit'
+		},
+		{
+			text: 'Delete',
+			callback_data: 'delete'
+		}
+	]
+]
 
-	for (let i = 0; i < 5; i++) {
-		results.push({
-			type: 'article',
-			id: i.toString(),
-			title: 'Title ' + i, 
-			input_message_content: {
-				message_text: `Article ${i+1}`
-			}
-		})
+bot.on('callback_query', query => {
+	const { chat, message_id, text } = query.message
+
+	switch (query.data) {
+		case 'forward':
+			bot.forwardMessage(chat.id, chat.id, message_id)
+			break
+		case 'reply':
+			bot.sendMessage(chat.id, `Отвечаем на сообщение`, {
+				reply_to_message_id: message_id
+			})
+			break
 	}
+	bot.answerCallbackQuery({
+		callback_query_id: query.id
+	})
+})
 
-	bot.answerInlineQuery(query.id, results, {
-		cache_time: 0
+bot.onText(/\/start/, (msg, [source, match]) => {
+	const chatId = msg.chat.id
+
+	bot.sendMessage(chatId, 'Keyboard', {
+		reply_markup: {
+			inline_keyboard
+		}
 	})
 })
